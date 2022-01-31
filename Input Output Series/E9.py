8f97ca0fb6e05b6f24c8783603d1c1049a9e028a
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-01-30 22:52:39 -0700

    mor

--- Input Output Series/E9.py
+++ Input Output Series/E9.py
@@ -1,15 +1,21 @@
 from random import randint
 
-
+checkempty = open('E9_file_check.txt', 'w')
+checkempty.write('')
+checkempty.close()
 
 chance = randint(0,1)
 
-
 if chance :
     checkempty = open('E9_file_check.txt', 'w')
-    checkempty.write('superduper')
+    checkempty.write('stuff')
     checkempty.close()
 
 checkempty = open('E9_file_check.txt', 'r')
-
+checkempty_line = checkempty.read()
 checkempty.close()
+
+if not len(checkempty_line) :
+    print('File is empty')
+else :
+    print('File is not empty')
