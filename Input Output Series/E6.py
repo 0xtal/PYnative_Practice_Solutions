577d1691b48685618aa34deb344b8e8f580763de
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-01-30 22:02:10 -0700

    file operations

--- Input Output Series/E6.py
+++ Input Output Series/E6.py
@@ -1,1 +1,18 @@
-print
+source = open('E6_data_in.txt', 'r')
+source_line = source.read()
+source.close()
+
+print(source_line)
+print('\n',end='')
+source_line = source_line.replace('line5\n','')
+
+destination = open('E6_data_out.txt', 'w')
+destination.write(source_line)
+destination.close()
+
+destination = open('E6_data_out.txt', 'r')
+for line in destination :
+    print(line, end = '')
+
+destination.close()
+
