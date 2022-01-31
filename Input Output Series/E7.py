6935f5a3222a95989b3b46fb3726aeebe0ecc03d
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-01-30 22:18:48 -0700

    more prac

---
+++ Input Output Series/E7.py
@@ -0,0 +1,2 @@
+item1, item2, item3 = input('Give me three strings:').split()
+print('item1:', item1, '\nitem2:', item2, '\nitem3:', item3)
---
+++ Input Output Series/E8.py
@@ -0,0 +1,5 @@
+totalMoney = 1000
+quantity = 3
+price = 450
+
+print('I have',totalMoney,'dollars so I can buy',quantity,'football for','%.2f' % price,'dollars.')
