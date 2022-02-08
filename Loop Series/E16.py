f1cea2f9307dce082b5de068b8cf00d257812c6a
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-02-07 20:50:32 -0700

    Create E16.py

---
+++ Loop Series/E16.py
@@ -0,0 +1,8 @@
+def cube(base) :
+    return (base ** 3)
+
+
+max = int(input('Give a number: ')) 
+for i in range(1 ,max) :
+    print('Current Number is :', i, end = ' ')
+    print('and the cube is', cube(i))
