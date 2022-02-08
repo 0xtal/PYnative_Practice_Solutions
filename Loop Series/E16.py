6d2a2e1da3bda361bf5ee9d6c453bd0dd8e8d439
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-02-07 20:52:34 -0700

    fix

--- Loop Series/E16.py
+++ Loop Series/E16.py
@@ -3,6 +3,6 @@ def cube(base) :
 
 
 max = int(input('Give a number: ')) 
-for i in range(1 ,max) :
+for i in range(1 ,max + 1) :
     print('Current Number is :', i, end = ' ')
     print('and the cube is', cube(i))
