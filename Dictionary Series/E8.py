227ed99411751ff2c7540361f2e420092e273fc6
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-02-26 13:41:50 -0700

    '8'

---
+++ Dictionary Series/E8.py
@@ -0,0 +1,20 @@
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+
+sample_dict.update(location = sample_dict['city'])
+sample_dict.pop('city')
+print(sample_dict)
+
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+
+sample_dict['location'] = sample_dict.pop('city')
+print(sample_dict)
