342156d51baec318cf5543c34ea2d1d92e93f2cb
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-03-03 21:57:25 -0700

    '10

---
+++ DateTime Series/E10.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+# 2020-02-25
+date_1 = datetime(2020, 2, 25)
+
+# 2020-09-17
+date_2 = datetime(2020, 9, 17)
+
+timedelta = date_2 - date_1
+relativedelta = date_2 - date_1
+print(timedelta.days, 'days')
+print(relativedelta.days, 'days')
