ef7a1478350a81a4d50d1c65f228f4bfbd4ddffd
Kevin Fletcher <92233104+0xtal@users.noreply.github.com>
2022-03-03 21:51:36 -0700

    '9'

---
+++ DateTime Series/E9.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+# 2020-02-25
+given_date = datetime(2020, 2, 25).date()
+
+new_date = given_date + relativedelta(months = 4)
+
+print(new_date)
